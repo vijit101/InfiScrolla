ef5dc9728dfd1eea0083c9fc75fdb5f926fd5131
Shubham <40598861+vijit101@users.noreply.github.com>
2018-09-16 16:45:26 +0530

    InfiScrolla Commit 2 (Added 2 new scripts)
    Has Camera Actor and ceiling actor scripts

---
+++ Assets/Scripts/CameraActor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraActor : MonoBehaviour {
+    private Camera cam;
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+    public float leftbound()
+    {
+        return cam.transform.position.x - cam.aspect * cam.orthographicSize;
+    }
+    public float rightbound()
+    {
+        return cam.transform.position.x + cam.aspect * cam.orthographicSize;
+    }
+    public bool isleft(float Worldx)
+    {
+        return Worldx < leftbound();
+    }
+    public bool isright(float Worldx)
+    {
+        return Worldx > rightbound();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CeilingActor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider2D))]
+public class CeilingActor : MonoBehaviour {
+    BoxCollider2D box2d;
+    private void Awake()
+    {
+        box2d = GetComponent<BoxCollider2D>();
+    }
+    public Vector2 Toprightceiling()
+    {
+        return box2d.bounds.max;
+    }
+    public Vector2 Bottomleftceiling()
+    {
+        return box2d.bounds.min;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
