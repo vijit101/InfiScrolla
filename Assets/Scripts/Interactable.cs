d43e598143f14ac77d665be78753f41a8be52287
Shubham <40598861+vijit101@users.noreply.github.com>
2018-09-26 20:41:19 +0530

    InfiScrolla Commit 3 (Added 2 new scripts)
    Script interactable and interval System (semi made )

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    BoxCollider2D interactableboxcollider;
+    Rigidbody2D interactablergbd2D;
+    private void Awake()
+    {
+        interactableboxcollider = GetComponent<BoxCollider2D>();
+        interactablergbd2D = GetComponent<Rigidbody2D>();
+    }
+    public void moveinteractable(float amount)
+    {
+        interactablergbd2D.MovePosition(interactablergbd2D.position + new Vector2(amount, 0));
+    }
+    public float getinteractablemaxX()
+    {
+        return interactableboxcollider.bounds.max.x;
+    }
+    public float getinteractableminX()
+    {
+        return interactableboxcollider.bounds.min.x;
+    }
+    public float getmaxtop()
+    {
+        return interactableboxcollider.bounds.max.y - transform.position.y;
+    }
+    public float getmaxbottom()
+    {
+        return transform.position.y - interactableboxcollider.bounds.min.y;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/IntervalSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntervalSystem : MonoBehaviour {
+    List<Interactable> interactablelist;
+    public float movespeed;
+    CameraActor cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    struct Entity
+    {
+        public Interactable interactableobj;
+    }
+    Entity[] entities;
+    private void Awake()
+    {
+        interactablelist = new List<Interactable>();
+    }
+    private void mover(float amt)
+    {
+        for (int i = 0; i < interactablelist.Count; i++)
+        {
+            interactablelist[i].moveinteractable(amt);
+        }
+    }
+    private void getpendinginteractables()
+    {
+        int pending = 0;
+        for (int i = 0; i < interactablelist.Count; i++)
+        {
+            if (cam.isright(interactablelist[i].getinteractableminX()))
+            {
+                ++pending;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        float moveamount = -1 * movespeed * Time.deltaTime;
+        mover(moveamount);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
